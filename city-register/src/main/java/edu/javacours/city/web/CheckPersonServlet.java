0ef8bdd2ca32dcb4747087d19eda90705e742209
alex <alex-vav@list.ru>
2022-06-19 22:11:05 +0300

    Release 98 simple http response

---
+++ city-register/src/main/java/edu/javacours/city/web/CheckPersonServlet.java
@@ -0,0 +1,53 @@
+package edu.javacours.city.web;
+
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.annotation.WebServlet;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name ="CheckPersonServlet", urlPatterns = {"/checkPerson1"})
+//@WebServlet("/checkPerson")
+public class CheckPersonServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletRequestvletResponse resp) throws ServletException, IOException {
+////        super.doGet(req, resp);
+//        resp.getWriter().println("Get CheckPerson - called");
+//    }
+
+
+}
+
