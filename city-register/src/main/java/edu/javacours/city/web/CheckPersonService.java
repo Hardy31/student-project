723e1a80f3454a90738f4180b21d076a01aeda0d
alex <alex-vav@list.ru>
2022-07-21 23:47:18 +0300

    repeated lesson 97 best version

---
+++ city-register/src/main/java/edu/javacours/city/web/CheckPersonService.java
@@ -0,0 +1,24 @@
+//package edu.javacours.city.web;
+//
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import javax.ws.rs.GET;
+//import javax.ws.rs.Path;
+//
+//@Path("/check")
+//public class CheckPersonService {
+//
+////    private static  final Logger logger = LoggerFactory.getLogger(CheckPersonService.class);
+//
+////    public CheckPersonService() {
+////        logger.info("CheckPersonService -  IMPLEMENT!!!!!!!");
+////    }
+//
+//    @GET
+//    public  String checkPerson(){
+////        logger.info("CheckPersonService -  method checkPerson!!!!!!!!!");
+//        return "Simpl String";
+//    }
+//}
