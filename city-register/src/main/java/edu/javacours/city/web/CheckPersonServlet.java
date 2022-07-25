5102149c6e20e9e49f11751484a45691fa20bb34
alex <alex-vav@list.ru>
2022-07-25 22:54:22 +0300

    repeated lesson 100 best version

--- city-register/src/main/java/edu/javacours/city/web/CheckPersonServlet.java
+++ city-register/src/main/java/edu/javacours/city/web/CheckPersonServlet.java
@@ -1,28 +1,11 @@
 package edu.javacours.city.web;
 
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.annotation.WebServlet;
-//import jakarta.servlet.http.HttpServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-
 import edu.javacours.city.dao.PersonCheckDao;
 import edu.javacours.city.domain.PersonRequest;
 import edu.javacours.city.domain.PersonResponse;
 import edu.javacours.city.exception.PersonCheckException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.time.LocalDate;
@@ -30,9 +13,27 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
+//@WebServlet(name ="CheckPersonServlet", urlPatterns = {"/checkPerson"}, loadOnStartup = 1)
 @WebServlet(name ="CheckPersonServlet", urlPatterns = {"/checkPerson"})
-//@WebServlet("/checkPerson")
+
 public class CheckPersonServlet extends HttpServlet {
+
+    private PersonCheckDao dao;
+    private static  final Logger logger = LoggerFactory.getLogger(CheckPersonServlet.class);
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+        logger.info("SERVER - service method call, regues param :  " + req.getParameter("surname"));
+    }
+
+    @Override
+    public void init() throws ServletException {
+        logger.info("SERVER is created");
+        dao = new PersonCheckDao();
+    }
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -52,9 +53,7 @@ public class CheckPersonServlet extends HttpServlet {
         pr.setApartment("121");
 
 
-        try {
-            PersonCheckDao dao = new PersonCheckDao();
-            PersonResponse ps = dao.checkPerson(pr);
+        try {PersonResponse ps = dao.checkPerson(pr);
 
             if (ps.isRegistered()){
 
